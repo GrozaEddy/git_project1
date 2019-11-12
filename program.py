f2d9ceca408bc92e669a800f03f3d89158f7152b
Эдуард <evoronin2002@yandex.ru>
2019-11-12 16:35:24 +0300

    New commit

--- program.py
+++ program.py
@@ -1,7 +1,9 @@
 def main():
     print('My first git program')
     print('and i change it every day')
-    
+    print('again')
+    print('UFO came and added')
+
 
 if __name__ == '__main__':
     main()
